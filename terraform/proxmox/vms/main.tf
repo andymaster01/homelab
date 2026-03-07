cf99855536a41048de2de7fe1a2cbbecd87f8e3a
Mauricio Rivera <mauricio.rivera@fiivestudio.com>
2026-03-07 14:00:05 -0500

    tf: migrate from single vm to multiple vms

--- terraform/proxmox/vms/main.tf
+++ terraform/proxmox/vms/main.tf
@@ -27,67 +27,69 @@ data "terraform_remote_state" "images" {
 }
 
 resource "proxmox_virtual_environment_file" "vendor_cloud_init" {
+  for_each     = var.vms
   content_type = "snippets"
   datastore_id = "local"
-  node_name    = var.proxmox_node
+  node_name    = each.value.node_name
 
   source_raw {
     data = templatefile("${path.module}/files/vendor-cloud-init.yaml", {
-      username = var.vm_username
+      username = each.value.username
     })
-    file_name = "${var.vm_name}-vendor-cloud-init.yaml"
+    file_name = "${each.key}-vendor-cloud-init.yaml"
   }
 }
 
 resource "proxmox_virtual_environment_vm" "vm" {
-  name      = var.vm_name
-  node_name = var.proxmox_node
-  vm_id     = var.vm_id
+  for_each  = var.vms
+  name      = each.key
+  node_name = each.value.node_name
+  vm_id     = each.value.vm_id
   started   = true
 
   agent { enabled = true }
 
   cpu {
-    cores   = 2
+    cores   = each.value.cpu_cores
     sockets = 1
     type    = "host"
   }
 
-  memory { dedicated = 8192 }
+  memory { dedicated = each.value.memory }
 
   disk {
-    datastore_id = var.proxmox_storage_vm
-    file_id      = data.terraform_remote_state.images.outputs.ubuntu_cloud_image_id
+    datastore_id = each.value.storage
+    file_id      = data.terraform_remote_state.images.outputs.ubuntu_cloud_image_ids[each.value.node_name]
     interface    = "virtio0"
-    size         = 16
+    size         = each.value.disk_size
     discard      = "on"
     file_format  = "raw"
   }
 
   network_device {
-    bridge = var.vm_bridge
+    bridge = each.value.bridge
     model  = "virtio"
   }
 
   operating_system { type = "l26" }
 
   initialization {
-    datastore_id        = var.proxmox_storage_vm
-    vendor_data_file_id = proxmox_virtual_environment_file.vendor_cloud_init.id
+    datastore_id        = each.value.storage
+    vendor_data_file_id = proxmox_virtual_environment_file.vendor_cloud_init[each.key].id
 
     ip_config {
       ipv4 {
-        address = var.vm_ip
-        gateway = var.vm_gateway
+        address = each.value.ip
+        gateway = each.value.gateway
       }
     }
 
     dns {
-      servers = [var.vm_dns]
+      servers = [each.value.dns]
     }
 
     user_account {
-      username = var.vm_username
+      username = each.value.username
       keys     = [trimspace(var.vm_ssh_public_key)]
     }
   }
--- terraform/proxmox/vms/terraform.tfvars
+++ terraform/proxmox/vms/terraform.tfvars
@@ -1,5 +1,16 @@
-proxmox_storage_vm = "local-lvm"
-vm_name            = "ubuntu-01"
-vm_id              = 200
-vm_ip              = "192.168.1.130/24"
-vm_gateway         = "192.168.1.1"
+vms = {
+  ubuntu-01 = {
+    node_name = "bahamut"
+    vm_id     = 200
+    ip        = "192.168.1.130/24"
+    gateway   = "192.168.1.1"
+    memory    = 8192
+  }
+  monitoring-01 = {
+    node_name = "eiko"
+    vm_id     = 201
+    ip        = "192.168.1.150/24"
+    gateway   = "192.168.1.1"
+    memory    = 2048
+  }
+}
